8dfcebe22ea6a81934d05d27dd54a68db8e2188d
aunuj <aunujain53@gmail.com>
2021-08-29 21:22:40 +0530

    backend php prep done

---
+++ assets/php/backend.php
@@ -0,0 +1,32 @@
+<?php
+
+// $server = 'localhost';
+// $username = 'root';
+// $password = '';
+// $database = 'contact';
+
+// $conn = mysqli_connect($server, $username, $password, $database);
+// if(!$conn){
+//     die("Error ". mysqli_connect_error());
+// }
+
+// if($_SERVER["REQUEST_METHOD"] == "POST"){
+//     $name = $_POST["name"];
+//     $email = $_POST["email"];
+//     $project = $_POST["project"];
+//     $msg = $_POST["msg"];
+    
+
+//     // $sql = "INSERT INTO `formtb` (`name`, `age`, `mobile`, `city`, `state`, `pin`, `education`, `mar_status`, `id`) VALUES 
+//     // ('$name', '$age', '$mobileNo', '$city', '$state', '$pincode', '$education', '$martialStatus', null)";
+//     // $result = mysqli_query($conn, $sql);
+
+//     if($result){
+//         header("location: thanks.html");//redirect page
+//     }else{
+//         header("location: form.php");//eror page
+//     }
+// }
+
+echo"excelent"
+?>
