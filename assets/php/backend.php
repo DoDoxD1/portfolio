64a1afd80c749b4e8af153d61bbb5e9370dadf86
aunuj <aunujain53@gmail.com>
2021-08-30 18:12:22 +0530

    backend done

--- assets/php/backend.php
+++ assets/php/backend.php
@@ -1,32 +1,39 @@
 <?php
 
-// $server = 'localhost';
-// $username = 'root';
-// $password = '';
-// $database = 'contact';
+$server = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'contact';
 
-// $conn = mysqli_connect($server, $username, $password, $database);
-// if(!$conn){
-//     die("Error ". mysqli_connect_error());
-// }
+$conn = mysqli_connect($server, $username, $password, $database);
+if(!$conn){
+    die("Error ". mysqli_connect_error());
+}
 
-// if($_SERVER["REQUEST_METHOD"] == "POST"){
-//     $name = $_POST["name"];
-//     $email = $_POST["email"];
-//     $project = $_POST["project"];
-//     $msg = $_POST["msg"];
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $project = $_POST["project"];
+    if(isset($_POST["msg"])){
+        $msg = $_POST["msg"];
+    }
+    else{
+        $msg = null;
+    }
     
 
-//     // $sql = "INSERT INTO `formtb` (`name`, `age`, `mobile`, `city`, `state`, `pin`, `education`, `mar_status`, `id`) VALUES 
-//     // ('$name', '$age', '$mobileNo', '$city', '$state', '$pincode', '$education', '$martialStatus', null)";
-//     // $result = mysqli_query($conn, $sql);
+    // $sql = "INSERT INTO `formtb` (`name`, `age`, `mobile`, `city`, `state`, `pin`, `education`, `mar_status`, `id`) VALUES 
+    // ('$name', '$age', '$mobileNo', '$city', '$state', '$pincode', '$education', '$martialStatus', null)";
 
-//     if($result){
-//         header("location: thanks.html");//redirect page
-//     }else{
-//         header("location: form.php");//eror page
-//     }
-// }
-
-echo"excelent"
+$sql = "INSERT INTO `contact`(`name`, `email`, `project`, `msg`) VALUES ('$name','$email','$project','$msg')";
+$result = mysqli_query($conn, $sql);
+    if($result){
+        echo"<script>alert('Hola! Your record is submitted. You will be redirected now.');window.location = '/portfolio/index.html';</script>";
+        
+    }else{
+        echo"<script>alert('Oh! An error occured! Your record not submitted');window.location = '/portfolio/index.html';</script>";
+    }
+    
+    // header("location:  \portfolio/index.html");
+}
 ?>
